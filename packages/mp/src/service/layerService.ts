f24e85066402ef15db510181a7dcfec47ce0c50c
xdoer <gotoanything@foxmail.com>
2022-03-19 16:43:10 +0800

    pref: immer

--- packages/mp/src/service/layerService.ts
+++ packages/mp/src/service/layerService.ts
@@ -1,5 +1,6 @@
 import StateBus from '@xdoer/state-bus'
 import React, { CSSProperties } from 'react'
+import { produce } from 'immer'
 
 export interface Layer<T> {
   visible: boolean
@@ -11,31 +12,51 @@ export interface Layer<T> {
 class LayerService<T> {
   state: StateBus<Layer<T>[]> = new StateBus<Layer<T>[]>([])
   zIndex = 10
+  watches = {}
 
   constructor(private init: T) {}
 
   open(data: T | React.ReactElement, key = 'default') {
     const _data = React.isValidElement(data) ? { content: data } : data
 
-    this.state.setState((prev) => {
-      const findIdx = prev.findIndex((i) => i.key === key)
-      const newObj: Layer<T> = {
-        model: { ...this.init, ..._data },
-        visible: true,
-        key,
-        zIndex: this.zIndex++,
-      }
+    this.state.setState(
+      produce((prev) => {
+        const idx = prev.findIndex((i) => i.key === key)
+        const newObj: Layer<T> = {
+          model: { ...this.init, ..._data },
+          visible: true,
+          key,
+          zIndex: this.zIndex++,
+        }
 
-      prev[findIdx === -1 ? prev.length : findIdx] = newObj
+        if (this.watches[key]) {
+          this.watches[key](newObj)
+        }
 
-      return [...prev]
-    })
+        prev[idx === -1 ? prev.length : idx] = newObj as any
+      }),
+    )
   }
 
   close(key = 'default') {
-    this.state.setState((prev) =>
-      prev.map((i) => ({ ...i, visible: key === i.key ? false : i.visible })),
+    this.state.setState(
+      produce((prev) => {
+        const idx = prev.findIndex((i) => i.key === key)
+        prev[idx].visible = false
+
+        if (this.watches[key]) {
+          this.watches[key](prev[idx])
+        }
+      }),
     )
+  }
+
+  watch(key: string, cb: (v: Layer<T>) => void) {
+    this.watches[key] = cb
+  }
+
+  unWatch(key: string) {
+    delete this.watches[key]
   }
 
   private i = 0
